5f77909358d5ebd1dbeec9426314f9a996c5f638
Sandro Paetzold <sandro@paetzold.co.uk>
2026-05-22 16:22:23 +0000

    Pluralsight "Go Fundamentals", Module 03

--- pluralsight/go-fundamentals/main.go
+++ pluralsight/go-fundamentals/main.go
@@ -5,11 +5,22 @@ import (
 	"strings"
 )
 
+type Student struct {
+	Name  string
+	Score int
+}
+
 func main() {
-	name := "Dent, Arthur"
-	score := 87
+
+	students := []Student{
+		{Name: "Dent, Arthur", Score: 87},
+		{Name: "MacMillan, Tricia", Score: 96},
+		{Name: "Prefect, Ford", Score: 64},
+	}
 
 	fmt.Println("Student scores")
 	fmt.Println(strings.Repeat("-", 14))
-	fmt.Println(name, score)
+	fmt.Println(students[0].Name, students[0].Score)
+	fmt.Println(students[1].Name, students[1].Score)
+	fmt.Println(students[2].Name, students[2].Score)
 }
