e65338c767d764326488ef9716535497d2ab0ed5
sandr0-p <70605028+sandr0-p@users.noreply.github.com>
2026-05-25 19:34:35 +0100

    Go fundamentals module 05 (#13)
    * Pluralsight "Go Fundamentals", Module 05
    
    * Pluralsight, "Go Fundamentals", Module 05

--- pluralsight/go-fundamentals/main.go
+++ pluralsight/go-fundamentals/main.go
@@ -28,19 +28,27 @@ func main() {
 
 		switch option {
 		case "1":
-			fmt.Println("Enter student name and score:")
-			var name, rawScore string
-			fmt.Scanln(&name, &rawScore)
-			s, _ := strconv.Atoi(rawScore)
-			students = append(students, Student{Name: name, Score: s})
+			students = addStudent(students)
 		case "2":
-			fmt.Println("Student scores")
-			fmt.Println("----------------")
-			for _, s := range students {
-				fmt.Println(s.Name, s.Score)
-			}
+			printReport(students)
 		case "q":
 			shouldContinue = false
 		}
+	}
+}
+
+func addStudent(students []Student) []Student {
+	fmt.Println("Enter student name and score:")
+	var name, rawScore string
+	fmt.Scanln(&name, &rawScore)
+	s, _ := strconv.Atoi(rawScore)
+	return append(students, Student{Name: name, Score: s})
+}
+
+func printReport(students []Student) {
+	fmt.Println("Student scores")
+	fmt.Println("----------------")
+	for _, s := range students {
+		fmt.Println(s.Name, s.Score)
 	}
 }
